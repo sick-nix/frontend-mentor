43feb4fb3d311e2896e44afaf479cd5ba90c087d
sick-nix <vnom.snak3@gmail.com>
2023-10-18 21:50:55 +0200

    implemented interactive-rating-component

--- interactive-rating-component/src/App.jsx
+++ interactive-rating-component/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+	const ratingValues = [1, 2, 3, 4, 5]
+	const [selectedRating, setSelectedRating] = useState(0)
+	const [submitted, setSubmitted] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			{!submitted ? (
+				<div className="container">
+					<div className="star-icon round-btn">
+						<img src="/icon-star.svg" />
+					</div>
+					<h2 className="header">How did we do?</h2>
+					<div className="content">
+						Please let us know how we did with your support request.
+						All feedback is appreciated to help us improve our
+						offering!
+					</div>
+					<div className="ratings">
+						{ratingValues.map((r) => (
+							<button
+								className={`rating round-btn selectedRating ${
+									selectedRating == r ? "selected" : ""
+								}`}
+								onClick={() => setSelectedRating(r)}
+							>
+								{r}
+							</button>
+						))}
+					</div>
+					<button
+						className="submit-btn"
+						onClick={() => setSubmitted(true)}
+					>
+						SUBMIT
+					</button>
+				</div>
+			) : (
+				<div className="container with-selected-rating">
+					<div className="illustration-thank-you">
+						<img src="/illustration-thank-you.svg" />
+					</div>
+					<div className="selected-rating">
+						You selected {selectedRating} out of 5
+					</div>
+					<h2 className="header">Thank you!</h2>
+					<div className="content">
+						We appreciate you taking the time to give a rating. If
+						you ever need more support, don’t hesitate to get in
+						touch!
+					</div>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default App
